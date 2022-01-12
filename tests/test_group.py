29992db275fa874b1f09dfa443ea66fe084e2fa6
mlev71 <max.adam.levinson@gmail.com>
2022-01-11 19:02:23 -0500

    added testing for validation logic

--- tests/test_group.py
+++ tests/test_group.py
@@ -6,6 +6,7 @@ import mds
 import unittest
 
 class TestUserView(unittest.TestCase):
+
 	def test_userview(self):
 		member = mds.UserView(
 			id = "ark:99999/testuser1",
@@ -15,12 +16,21 @@ class TestUserView(unittest.TestCase):
 
 		self.assertEqual(member.id, "ark:99999/testuser1")
 
+	def test_userview_id_validation(self):
+		with self.assertRaises(ValueError):
+			member = mds.UserView(
+				id = "99999/testuser1",
+				name = "test user1",
+				email = "testuser1@example.org"
+				)
+
+	def test_userview_json(self):
+		pass
+
 
 class TestGroup(unittest.TestCase):
 
-
-
-	def test_group(self):
+	def test_group_initilization(self):
 		owner = mds.UserView(
 			id = "ark:99999/testowner",
 			name = "test owner",
@@ -40,6 +50,11 @@ class TestGroup(unittest.TestCase):
 			members=[member]
 			)	
 
+	def test_group_id_validation(self):
+		pass
+
+	def test_group_json(self):
+		pass
 
 if __name__=="__main__":
 	unittest.main()
