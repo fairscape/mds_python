740fc2a9a553678372e837890e120f6f63952c78
mlev71 <max.adam.levinson@gmail.com>
2022-01-11 19:01:21 -0500

    implemented reusable validation methods for multiple models

--- mds/group.py
+++ mds/group.py
@@ -1,20 +1,25 @@
 from pydantic import BaseModel, Extra, validator, ValidationError
 from typing import List
 
+def validate_ark(guid: str) -> str:
+	if 'ark:' not in guid:
+		raise ValueError(f"ark: prefix not in identifier {guid}")
+	return guid
+
+
 class UserView(BaseModel):
 	id: str
 	objectType = "Person" 
 	name: str
 	email: str
 
-	@validator('id')
-	def id_ark_format(cls, v):
-		if 'ark:' not in v:
-			raise ValueError(f"ark: prefix not in identifier {v}")
-		return v
+	_validate_guid = validator('id', allow_reuse=True)(validate_ark)
+
 
 class Group(BaseModel, extra=Extra.allow):
 	id: str
 	name: str
 	owner: UserView
 	members: List[UserView]
+
+	_validate_guid = validator('id', allow_reuse=True)(validate_ark)
