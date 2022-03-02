a22eea56010bc010cbcedde29949977daa9b5964
mlev71 <max.adam.levinson@gmail.com>
2022-03-02 15:59:06 -0500

    modifying test_mongo.py

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -11,19 +11,23 @@ class TestMongoConfig(unittest.TestCase):
 
 	def setUp(self):
 		# check that a local mongo docker container exists
-		docker_client = docker.from_env()
+		# docker_client = docker.from_env()
 
-		container_list = docker_client.containers.list()
-		mongo_available = False
+		# container_list = docker_client.containers.list()
+		# mongo_available = False
 
-		for container in container_list:
-			container.name
+		# for container in container_list:
+		# 	container.name
+		pass
 
-		# ping
-		client = MongoConfig().connect()
-		client.ping()
-
-		
 
 	def test_connection_local(self):
 		# make sure connection exists and can be reached
+		client = MongoConfig().connect()
+		client.ping()
+
+	def test_client_creation(self):
+		pass
+
+	def tearDown(self):
+		pass
