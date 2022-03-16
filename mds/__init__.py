387aae3faf769a0ede19403fdeae2a5554a29ee7
mlev71 <max.adam.levinson@gmail.com>
2022-03-16 13:57:06 -0400

    Fixes to __init__.py was breaking test suite imports

--- mds/__init__.py
+++ mds/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ["group", "computation", "mongo", "software", "user", "utils"]
+from .computation import Computation
+from .fairscape import Fairscape
+from .group import Group
+from .software import Software
+from .user import User
+from .mongo import MongoConfig
+from .utils import UserCompactView, SoftwareCompactView, DatasetCompactView, OrganizationCompactView, ComputationCompactView, ProjectCompactView
+
+__all__ = [Computation, Fairscape, Group, Software, User, MongoConfig, UserCompactView, SoftwareCompactView, DatasetCompactView, OrganizationCompactView, ComputationCompactView, ProjectCompactView]
