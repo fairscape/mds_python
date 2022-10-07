3509e696851a550b5ada9ddb2156d7e4c2e88e6f
mlev71 <max.adam.levinson@gmail.com>
2022-10-07 12:47:50 -0400

    adding sqlite support for casbin

--- mds/database/casbin.py
+++ mds/database/casbin.py
@@ -1,16 +1,5 @@
-import casbin_pymongo_adapter
 import casbin
-from urllib.parse import quote_plus
-from mds.database.config import MONGO_URI, MONGO_USER, MONGO_PASS, MONGO_DATABASE
+import casbin_sqlalchemy_adapter
 
-# TODO create class for enforcer and specific adapters
-
-def GetEnforcer() -> casbin.Enforcer:
-	""" return the casbin enforcer """
-	adapter = casbin_pymongo_adapter.Adapter(
-		f"mongodb://{quote_plus(MONGO_USER)}:{quote_plus(MONGO_PASS)}@{MONGO_URI}:27017/", 
-		MONGO_DATABASE)
-
-	# TODO configure casbin model as parameter
-	e = casbin.Enforcer('./tests/model.conf', adapter)
-	return e
+adapter = casbin_sqlalchemy_adapter.Adapter('sqlite:///casbin.db')
+casbinEnforcer = casbin.Enforcer('./tests/model.conf', adapter)
