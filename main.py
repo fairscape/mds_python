06b1154c01dc5e105817ab86f0a5e522e508d793
mlev71 <max.adam.levinson@gmail.com>
2023-05-24 12:12:20 -0400

    adding configuration options to main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import argparse
 import uvicorn
 from os import environ
 from mds.database.minio import GetMinioConfig
 
 
 # TODO make sure configuration is valid, if not create configured minio bucket and mongo database
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", default='0.0.0.0')
+parser.add_argument("--port", default='8080')
 
 
 if __name__ == '__main__':
+
+    args = parser.parse_args()
 
     uvicorn.run(
         'mds.app:app', 
