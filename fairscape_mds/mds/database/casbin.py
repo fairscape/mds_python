9c6200f7ec523f15a1597a4bbb847bdbaec165bc
mlev71 <max.adam.levinson@gmail.com>
2024-02-02 14:23:10 -0500

    removed old configuration

--- fairscape_mds/mds/database/casbin.py
+++
@@ -1,5 +0,0 @@
-import casbin
-import casbin_sqlalchemy_adapter
-
-adapter = casbin_sqlalchemy_adapter.Adapter('sqlite:///casbin.db')
-casbinEnforcer = casbin.Enforcer('./mds/database/default_model.conf', adapter)
